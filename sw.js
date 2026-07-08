62a3d5eb9ce100d6d904e41e09805670ff3b0488
larry820227-cool <larry820227@gmail.com>
2026-07-09 02:33:34 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='filmlab-v57';
+const CACHE='filmlab-v571';
 const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
